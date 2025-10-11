f241f2828bf1726964cae3c994d0ef12b1644b63
Samantha <samantha@uppointinc.com>
2025-10-11 10:27:25 -0700

    adding home page and adding dist out reference to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     output: 'export',
+    distDir: 'out',
     images: {unoptimized:true},
-    basePath: '',
-    assetPrefix: '/stamp-calculator'
+    basePath: isProd ? '/stamp-calculator' : '',
+    assetPrefix: isProd ? '/stamp-calculator' : ''
 };
 
 export default nextConfig;
