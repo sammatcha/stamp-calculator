fa4389191689d6da9618f5f4b9bd56de38af7fc5
Samantha <samantha@uppointinc.com>
2025-10-11 10:43:43 -0700

    adding the basepath back in

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     output: 'export',
     images: {unoptimized:true},
-    // basePath: isProd ? '/stamp-calculator' : '',
-    // assetPrefix: isProd ? '/stamp-calculator/' : ''
+    basePath: isProd ? '/stamp-calculator' : '',
+    assetPrefix: isProd ? '/stamp-calculator/' : ''
 };
 
 export default nextConfig;
