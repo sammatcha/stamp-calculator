44dd4047971c432aab63da8ad25980cd0643e2b6
Samantha <samantha@uppointinc.com>
2025-10-11 10:41:48 -0700

    fix github pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,9 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
     output: 'export',
-    distDir: 'out',
     images: {unoptimized:true},
-    basePath: isProd ? '/stamp-calculator' : '',
-    assetPrefix: isProd ? '/stamp-calculator' : ''
+    // basePath: isProd ? '/stamp-calculator' : '',
+    // assetPrefix: isProd ? '/stamp-calculator/' : ''
 };
 
 export default nextConfig;
