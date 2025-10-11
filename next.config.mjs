0090c557958fe32be19021a00b545a82400eb201
Samantha <samantha@uppointinc.com>
2025-10-11 00:32:57 -0700

    fixing base path and image to show up

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
+    output: 'export',
     images: {unoptimized:true},
-    basePath: '/stamp-calculator',
-    assetPrefix: '/stamp-calculator/',
-
+    basePath: '',
+    assetPrefix: '/stamp-calculator'
 };
 
 export default nextConfig;
