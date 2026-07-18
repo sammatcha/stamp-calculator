3c2863e6923843180a7f711cd8936d8b286d419b
Samantha <samantha@uppointinc.com>
2026-07-17 18:20:11 -0700

    fixing the test for postage rates

--- tests/calculate-postage.test.js
+++ tests/calculate-postage.test.js
@@ -4,23 +4,23 @@ import { calculatePostage } from "../app/utils/calculatePostage";
 describe("calculatePostage", () =>{
     it("forever stamp for base stamp and when not machinable", () => {
         const result = calculatePostage(1, false);
-        expect(result.total).toBe(0.78);
+        expect(result.total).toBe(0.82);
         expect(result.breakdown[0].type).toBe('forever');
     });
 
     it("uses nonmachinable rate when checked", () =>{
         const result = calculatePostage(1, true);
-        expect(result.total).toBe(1.27);
+        expect(result.total).toBe(1.31);
         expect(result.breakdown[0].type).toBe("nonMachinable");
     });
 
     it("calculate nonmachinable with additional ounce", () => {
         const result = calculatePostage(2, true);
-        expect(result.total).toBe(1.27 + 0.29);
+        expect(result.total).toBe(1.31 + 0.29);
     })
 
     it("calculate forever with additional ounce", () =>{
         const result = calculatePostage(2,false);
-        expect(result.total).toBe(0.78 + 0.29);
+        expect(result.total).toBe(0.82 + 0.29);
     })
 })
